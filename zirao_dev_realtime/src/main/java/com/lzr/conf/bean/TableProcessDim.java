741dced5909bf3c142b7c23767a0a9c6d176a33f
33415 <3341517269@qq.com>
2025-05-05 20:29:44 +0800

    add init lvzirao

---
+++ zirao_dev_realtime/src/main/java/com/lzr/conf/bean/TableProcessDim.java
@@ -0,0 +1,24 @@
+package com.lzr.conf.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Package com.lzr.retail.com.lzy.stream.realtime.v1.realtime.bean.TableProcessDim
+ * @Author lv.zirao
+ * @Date 2025/4/8 8:47
+ * @description: TableProcessDim
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TableProcessDim {
+    String sourceTable;
+    String sinkTable;
+    String sinkColumns;
+    String sinkFamily;
+    String sinkRowKey;
+    String op;
+}
