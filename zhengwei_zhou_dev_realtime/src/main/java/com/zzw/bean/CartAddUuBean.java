ad330574e54854f93fe97b05eb27d5fe4ca6893c
周政卫 <14643151+zheng_wei18061094568@user.noreply.gitee.com>
2025-05-05 21:27:25 +0800

    add  init zhengwei_zhou

---
+++ zhengwei_zhou_dev_realtime/src/main/java/com/zzw/bean/CartAddUuBean.java
@@ -0,0 +1,20 @@
+package com.zzw.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Package com.lzy.retail.v1.realtime.bean.CartAddUuBean
+ * @Author zhengwei_zhou
+ * @Date 2025/4/8 8:44
+ * @description: CartAddUuBean
+ */
+
+@Data
+@AllArgsConstructor
+public class CartAddUuBean {
+    String stt;
+    String edt;
+    String curDate;
+    Long cartAddUuCt;
+}
