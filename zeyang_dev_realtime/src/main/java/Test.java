3d1a7726483d9982742b6f443e18c17e7abae99c
jzyayang <jzyayang123>
2025-05-05 14:11:26 +0800

    base58 Test

---
+++ zeyang_dev_realtime/src/main/java/Test.java
@@ -0,0 +1,16 @@
+import com.stream.common.utils.Base58;
+
+import java.util.Arrays;
+
+/**
+ * @Package PACKAGE_NAME.Test
+ * @Author ayang
+ * @Date 2025/5/5 14:09
+ * @description: test
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.err.println(Arrays.toString(Base58.decode("11")));
+    }
+}
