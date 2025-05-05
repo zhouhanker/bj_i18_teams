1e8ce9b761f2c3a10e01e4343af7f24ccd691164
zsf121381 <807561910@qq.com>
2025-05-05 19:06:59 +0800

    add shuaifei_zhao realtime

---
+++ shuaifei_zhao_dev_realtime/src/main/java/com/zsf/retail_v1/realtime/dim/TableProcessFunction.java
@@ -0,0 +1,104 @@
+package com.zsf.retail_v1.realtime.dim;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zsf.retail_v1.realtime.util.JdbcUtil;
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.sql.Connection;
+import java.util.*;
+
+/**
+ * @Package com.zsf.retail_v1.realtime.dim.TableProcessFunction
+ * @Author zhao.shuai.fei
+ * @Date 2025/4/23 20:38
+ * @description:
+ */
+public class TableProcessFunction extends BroadcastProcessFunction<JSONObject, JSONObject, Tuple2<JSONObject,JSONObject>> {
+    private MapStateDescriptor<String, JSONObject> mapStateDescriptor;
+    private Map<String,JSONObject> configMap = new HashMap<>();
+
+    public TableProcessFunction(MapStateDescriptor<String, JSONObject> mapStateDescriptor) {
+        this.mapStateDescriptor = mapStateDescriptor;
+    }
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        //将配置表中的配置信息预加载到程序configMap中
+        Connection mySQLConnection = JdbcUtil.getMySQLConnection();
+        List<JSONObject> JSONObjectList = JdbcUtil.queryList(mySQLConnection, "select * from sx_002_v2.table_process_dim", JSONObject.class, true);
+        for (JSONObject JSONObject : JSONObjectList) {
+            configMap.put(JSONObject.getString("sourceTable"),JSONObject);
+        }
+        JdbcUtil.closeMySQLConnection(mySQLConnection);
+    }
+
+    //处理主流业务数据               根据维度表名到广播状态中读取配置信息，判断是否为维度
+    @Override
+    public void processElement(JSONObject jsonObj, BroadcastProcessFunction<JSONObject, JSONObject, Tuple2<JSONObject,JSONObject>>.ReadOnlyContext ctx, Collector<Tuple2<JSONObject,JSONObject>> out) throws Exception {
+        //主流 状态变量
+        ReadOnlyBroadcastState<String, JSONObject> state = ctx.getBroadcastState(mapStateDescriptor);
+
+        //获取主流内的table表名
+        String table = jsonObj.getJSONObject("source").getString("table");
+
+        //根据表名先到广播状态中获取对应的配置信息，如果没有找到对应的配置，再尝试到configMap中获取
+        JSONObject JSONObject = null;
+
+        if ((JSONObject = state.get(table)) != null
+                || (JSONObject =  configMap.get(table)) != null) {
+            //如果根据表名获取到了对应的配置信息，说明当前处理的是维度数据
+
+            // 将维度数据继续向下游传递(只需要传递data属性内容即可)
+            JSONObject dataJsonObj = jsonObj.getJSONObject("after");
+
+            //在向下游传递数据前，过滤掉不需要传递的属性
+            String sinkColumns = JSONObject.getString("sink_columns");
+            deleteNotNeedColumns(dataJsonObj, sinkColumns);
+
+            //在向下游传递数据前，补充对维度数据的操作类型属性
+            String op = jsonObj.getString("op");
+            dataJsonObj.put("type", op);
+
+            out.collect(Tuple2.of(dataJsonObj, JSONObject));
+        }
+    }
+
+    //处理广播流配置信息    将配置数据放到广播状态中或者从广播状态中删除对应的配置  k:维度表名     v:一个配置对象
+    @Override
+    public void processBroadcastElement(JSONObject tp, BroadcastProcessFunction<JSONObject, JSONObject, Tuple2<JSONObject,JSONObject>>.Context ctx, Collector<Tuple2<JSONObject,JSONObject>> out) throws Exception {
+        //获取 op 操作状态
+        String op = tp.getString("op");
+        //获取 要写入状态算子的 数据
+        String sourceTable = tp.getString("source_table");
+        //获取 状态算子
+        BroadcastState<String, JSONObject> state = ctx.getBroadcastState(mapStateDescriptor);
+        //判断 是否是删除操作
+        if ("d".equals(op)) {
+            //删除 状态算子 里对应的key的数据
+            state.remove(sourceTable);
+        } else {
+            //非删除 状态算子 里添加对应的数据
+            state.put(sourceTable, tp);
+            configMap.put(sourceTable,tp);
+        }
+    }
+
+    //过滤掉不需要传递的字段
+    //dataJsonObj  {"tm_name":"Redmi","create_time":"2021-12-14 00:00:00","logo_url":"555","id":1}
+    //sinkColumns  id,tm_name
+    private static void deleteNotNeedColumns(JSONObject dataJsonObj, String sinkColumns) {
+        if(sinkColumns!=null){
+            List<String> columnList = Arrays.asList(sinkColumns.split(","));
+
+            Set<Map.Entry<String, Object>> entrySet = dataJsonObj.entrySet();
+
+            entrySet.removeIf(entry-> !columnList.contains(entry.getKey()));
+        }
+    }
+}
---
+++ shuaifei_zhao_dev_realtime/src/main/java/com/zsf/retail_v1/realtime/dwd/DwdTradeOrderCancelDetail.java
@@ -0,0 +1,144 @@
+package com.zsf.retail_v1.realtime.dwd;
+
+
+import com.zsf.retail_v1.realtime.constant.Constant;
+import com.zsf.retail_v1.realtime.util.SQLUtil;
+import lombok.SneakyThrows;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+
+/**
+ * &#064;Package  com.zsf.retail.v1.realtime.dwd.DwdTradeOrderCancelDetail
+ * &#064;Author  zhao.shuai.fei
+ * &#064;Date  2025/4/10 20:26
+ * &#064;description:取消订单事实表
+ */
+public class DwdTradeOrderCancelDetail {
+    @SneakyThrows
+    public static void main(String[] args) {
+        //流处理
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 并行度，
+        env.setParallelism(4);
+        // flink sql初始化
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        //TODO 设置状态的保留时间
+//        tableEnv.getConfig().setIdleStateRetention(Duration.ofSeconds(30 * 60 + 5));
+        //TODO 从kafka的topic_db主题中读取数据
+        //TODO 从kafka的topic_db主题中读取数据 创建动态表
+        tableEnv.executeSql("create table topic_db(\n" +
+                "    `before` map<string,string>,\n" +
+                "    `after` map<string,string>,\n" +
+                "    `source` map<string,string>,\n" +
+                "    `op` string,\n" +
+                "    `ts_ms` BIGINT,\n" +
+                "    proc_time as proctime()\n" +
+                ")WITH (\n" +
+                "  'connector' = 'kafka',\n" +
+                "  'topic' = 'topic_db',\n" +
+                "  'properties.bootstrap.servers' = 'cdh01:9092',\n" +
+                "  'properties.group.id' = 'testGroup01',\n" +
+                "  'scan.startup.mode' = 'earliest-offset',\n" +
+                "  'format' = 'json'\n" +
+                ")");
+//        tableEnv.executeSql("select * from topic_db limit 5").print();
+
+        //TODO 过滤出取消订单行为
+        Table orderCancel = tableEnv.sqlQuery("select " +
+                " `after`['id'] id, " +
+                " `after`['operate_time'] operate_time, " +
+                " `source`['ts_ms'] ts_ms " +
+                "from topic_db " +
+                "where `source`['table']='order_info' " +
+                "and `op`='u' " +
+                "and `before`['order_status']='1001' " +
+                "and `after`['order_status']='1003' ");
+        tableEnv.createTemporaryView("order_cancel", orderCancel);
+//        orderCancel.execute().print();
+
+        //TODO 从下单事实表中获取下单数据
+        tableEnv.executeSql(
+                "create table dwd_trade_order_detail(" +
+                        "id string," +
+                        "order_id string," +
+                        "user_id string," +
+                        "sku_id string," +
+                        "sku_name string," +
+                        "province_id string," +
+                        "activity_id string," +
+                        "activity_rule_id string," +
+                        "coupon_id string," +
+                        "date_id string," +
+                        "create_time string," +
+                        "sku_num string," +
+                        "split_original_amount string," +
+                        "split_activity_amount string," +
+                        "split_coupon_amount string," +
+                        "split_total_amount string," +
+                        "ts_ms bigint " +
+                        ") WITH (\n" +
+                        "  'connector' = 'kafka',\n" +
+                        "  'topic' = 'dwd_trade_order_detail',\n" +
+                        "  'properties.bootstrap.servers' = 'cdh01:9092',\n" +
+                        "  'properties.group.id' = 'dwd_trade_order_cancel03',\n" +
+                        "  'scan.startup.mode' = 'earliest-offset',\n" +
+                        "  'format' = 'json'\n" +
+                        ")");
+//        tableEnv.sqlQuery("select * from dwd_trade_order_detail").execute().print();
+
+        //TODO 将下单事实表和取消订单表进行关联
+        Table result = tableEnv.sqlQuery(
+                "select  " +
+                        "od.id," +
+                        "od.order_id," +
+                        "od.user_id," +
+                        "od.sku_id," +
+                        "od.sku_name," +
+                        "od.province_id," +
+                        "od.activity_id," +
+                        "od.activity_rule_id," +
+                        "od.coupon_id," +
+                        "date_format(oc.operate_time, 'yyyy-MM-dd') order_cancel_date_id," +
+                        "oc.operate_time," +
+                        "od.sku_num," +
+                        "od.split_original_amount," +
+                        "od.split_activity_amount," +
+                        "od.split_coupon_amount," +
+                        "od.split_total_amount," +
+                        "oc.ts_ms " +
+                        "from dwd_trade_order_detail od " +
+                        "join order_cancel oc " +
+                        "on od.order_id=oc.id ");
+//        result.execute().print();
+
+        //TODO 将关联的结果写到kafka主题中
+        tableEnv.executeSql(
+                "create table "+ Constant.TOPIC_DWD_TRADE_ORDER_CANCEL+"(" +
+                        "id string," +
+                        "order_id string," +
+                        "user_id string," +
+                        "sku_id string," +
+                        "sku_name string," +
+                        "province_id string," +
+                        "activity_id string," +
+                        "activity_rule_id string," +
+                        "coupon_id string," +
+                        "date_id string," +
+                        "cancel_time string," +
+                        "sku_num string," +
+                        "split_original_amount string," +
+                        "split_activity_amount string," +
+                        "split_coupon_amount string," +
+                        "split_total_amount string," +
+                        "ts_ms string ," +
+                        "PRIMARY KEY (id) NOT ENFORCED " +
+                        ")" + SQLUtil.getUpsertKafkaDDL(Constant.TOPIC_DWD_TRADE_ORDER_CANCEL));
+
+        result.executeInsert(Constant.TOPIC_DWD_TRADE_ORDER_CANCEL);
+
+//        env.execute("dwd05");
+    }
+}
