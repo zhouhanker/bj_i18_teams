21c12ab9cfe7af55b2576aac07bf4b35b058e067
zrb0604 <1130109940@qq.com>
2025-05-06 15:22:50 +0800

    delete projects

--- zhaohua_liu_dev_realtime/stream-dev-v1/dosc/shell/flume.sh
+++
@@ -1,5 +0,0 @@
-sudo flume-ng agent \
---conf /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/etc/flume-ng/conf.empty/ \
---conf-file /opt/mock/flume/flume_file_kafka.conf \
---name a1 \
--Dflume.root.logger=INFO,console
